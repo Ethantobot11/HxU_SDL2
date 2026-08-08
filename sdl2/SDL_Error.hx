48a94dda7b7d658138d234e3c542c486f8dff527
Ethantobot <ethantobot7@gmail.com>
2026-08-08 12:52:59 +0200

    Update SDL_Error.hx

--- sdl2/SDL_Error.hx
+++ sdl2/SDL_Error.hx
@@ -1,5 +1,10 @@
 package sdl2;
 
+import cxx.num.*;
+import cxx.Ptr;
+import cxx.VoidPtr;
+import cxx.Ptr;
+
 @:cppInclude("SDL2/SDL_error.h") 
 @:include("SDL2/SDL_error.h")
 extern class SDL_Error {
