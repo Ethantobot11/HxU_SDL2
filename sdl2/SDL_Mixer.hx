fbfba2a0ba6ded0f3864498dd7d284d5786442dc
Ethantobot <ethantobot7@gmail.com>
2026-08-08 12:52:44 +0200

    Update SDL_Mixer.hx

--- sdl2/SDL_Mixer.hx
+++ sdl2/SDL_Mixer.hx
@@ -1,5 +1,10 @@
 package sdl2;
 
+import cxx.num.*;
+import cxx.Ptr;
+import cxx.VoidPtr;
+import cxx.Ptr;
+
 @:cppInclude("SDL2/SDL_mixer.h")
 @:include("SDL2/SDL_mixer.h")
 @:native("MIX_InitFlags")
