9db94d9a95758445122d57b220edfacbcd380610
Ethantobot <ethantobot7@gmail.com>
2026-08-08 13:45:12 +0200

    Update and rename Import.hx to import.hx

--- sdl2/import.hx
+++ sdl2/import.hx
@@ -11,10 +11,3 @@ import sdl2.SDL_Pixels.SDL_Color;
 
 import cxx.*;
 import cxx.num.*;
-import cxx.std.*;
-import cxx.std.ios.*;
-import cxx.std.chronos.*;
-import cxx.io.*;
-import cxxcompiler.*;
-import haxe.*;
-import haxe.ds.*;
